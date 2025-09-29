4efdb534d1e1edaabf0f4255fda5ac94a2332303
Florian Otto <florian.otto@mindbird.de>
2025-09-30 00:27:42 +0200

    add nuliga_hessen_group_id field and update palettes for game_plan and result_table

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -3,7 +3,7 @@
 declare(strict_types=1)
 
 // Add new fields
-$GLOBALS['TL_DCA']['tl_content']['fields']['nu_liga_hessen_group_id'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['nuliga_hessen_group_id'] = [
     'exclude' => true,
     'inputType' => 'text',
     'eval' => [
@@ -16,3 +16,13 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['nu_liga_hessen_group_id'] = [
         'default' => ''
     ]
 ];
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['game_plan'] = '
+    {type_legend},type,headline;
+    {nuliga_hessen_legend},nuliga_hessen_group_id
+';
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['result_table'] = '
+    {type_legend},type,headline;
+    {nuliga_hessen_legend},nuliga_hessen_group_id
+';
