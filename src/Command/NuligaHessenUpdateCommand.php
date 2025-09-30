32594872d9b845700d98ad319d81d42b5b5148bb
Florian Otto <florian.otto@mindbird.de>
2025-09-30 12:27:23 +0200

    refactor NuligaHessenService and NuligaHessenUpdateCommand to use string for nuligaClubId and enhance output formatting

--- src/Command/NuligaHessenUpdateCommand.php
+++ src/Command/NuligaHessenUpdateCommand.php
@@ -5,7 +5,9 @@ namespace Mindbird\ContaoNuligaHessen\Command;
 use Mindbird\ContaoNuligaHessen\Service\NuligaHessenService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(name: 'nuliga-hessen:update', description: 'Update data from NuLiga Hessen')]
 class NuligaHessenUpdateCommand extends Command
@@ -15,9 +17,10 @@ class NuligaHessenUpdateCommand extends Command
         parent::__construct();
     }
 
-    public function __invoke(OutputInterface $output): int
+    public function __invoke(OutputInterface $output, InputInterface $input): int
     {
-        $output->info('Starting NuLiga Hessen update...');
+        $symfonyStyle = new SymfonyStyle($input, $output);
+        $symfonyStyle->info('Starting NuLiga Hessen update...');
         /*
          * Gather group IDs from the db
          * Fetch data from NuLiga for each group ID
@@ -25,12 +28,12 @@ class NuligaHessenUpdateCommand extends Command
          */
         $groupIds = $this->nuligaHessenService->getUsedGroupIds();
         foreach ($groupIds as $groupId) {
-            $output->writeln('Working on group ID: ' . $groupId['group_id']);
+            $symfonyStyle->writeln('Working on group ID: ' . $groupId['group_id']);
             try {
                 $data = $this->nuligaHessenService->fetchGroupDataFromApi($groupId);
-                $output->success('Successfully updated group ID: ' . $groupId['group_id']);
+                $symfonyStyle->success('Successfully updated group ID: ' . $groupId['group_id']);
             } catch (\Exception $e) {
-                $output->error('Error updating group ID ' . $groupId['group_id'] . ': ' . $e->getMessage());
+                $symfonyStyle->error('Error updating group ID ' . $groupId['group_id'] . ': ' . $e->getMessage());
             }
         }
         return Command::SUCCESS;
--- src/Service/NuligaHessenService.php
+++ src/Service/NuligaHessenService.php
@@ -7,7 +7,7 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class NuligaHessenService
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager, private readonly HttpClientInterface $client, private readonly string $nuligaApiToken, private readonly int $nuligaClubId)
+    public function __construct(private readonly EntityManagerInterface $entityManager, private readonly HttpClientInterface $client, private readonly string $nuligaApiToken, private readonly string $nuligaClubId)
     {
 
     }
@@ -24,7 +24,7 @@ class NuligaHessenService
     {
         $url = 'https://rs.handball.app/api.php/v1/federation/hhv/club/' . $this->nuligaClubId . '/group/' . $groupId;
         $response = $this->client->request('GET', $url, [
-            'auth_bearer' => $this->nuligaApiKey,
+            'auth_bearer' => $this->nuligaApiToken,
         ]);
         if ($response->getStatusCode() !== 200) {
             throw new \RuntimeException('Failed to fetch data from NuLiga API for group ID ' . $groupId);
