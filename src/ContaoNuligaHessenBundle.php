1ab5a442c1b17c814c915c0ba515b86a4f983a60
Florian Otto <florian.otto@mindbird.de>
2025-09-30 10:41:23 +0200

    add loadExtension method to ContaoNuligaHessenBundle and update services.yaml configuration

--- src/ContaoNuligaHessenBundle.php
+++ src/ContaoNuligaHessenBundle.php
@@ -2,9 +2,15 @@
 
 namespace Mindbird\ContaoNuligaHessen;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ContaoNuligaHessenBundle extends AbstractBundle
 {
-
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/config.yaml');
+        $container->import('../config/services.yaml');
+    }
 }
