f7a31fdbf326e553e43ea181a7d2a32e1f555c97
Florian Otto <florian.otto@mindbird.de>
2025-09-30 23:48:14 +0200

    refactor NuligaHessenCron to handle cron execution scope and return a promise

--- src/Cron/NuligaHessenCron.php
+++ src/Cron/NuligaHessenCron.php
@@ -4,13 +4,26 @@ namespace Mindbird\ContaoNuligaHessen\Cron;
 
 use Contao\CoreBundle\Cron\Cron;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
+use Contao\CoreBundle\Exception\CronExecutionSkippedException;
+use Contao\CoreBundle\Util\ProcessUtil;
+use GuzzleHttp\Promise\PromiseInterface;
 
 #[AsCronJob('*/5 * * * *')]
 class NuligaHessenCron
 {
-    public function __invoke()
+    public function __construct(private ProcessUtil $processUtil) {}
+
+    public function __invoke(string $scope): PromiseInterface
     {
 
+        if (Cron::SCOPE_WEB === $scope) {
+            throw new CronExecutionSkippedException();
+        }
+
+
+        return $this->processUtil->createPromise(
+            $this->processUtil->createSymfonyConsoleProcess('hessen-nuliga:update')
+        );
     }
 
 }
