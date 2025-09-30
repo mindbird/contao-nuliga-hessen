191922e566e532c723411bc86d1f4a16130afe49
Florian Otto <florian.otto@mindbird.de>
2025-09-30 10:33:29 +0200

    update load method signature to include return type and remove unused import

--- src/DependencyInjection/ContaoNuligaHessenExtension.php
+++ src/DependencyInjection/ContaoNuligaHessenExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 class ContaoNuligaHessenExtension extends Extension
 {
 
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('config.yaml');
